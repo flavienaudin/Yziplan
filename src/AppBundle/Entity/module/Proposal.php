0c7f4b7642e862c5dfc59765c056f0bd8ba8c66e
Patrick <patrickmbox@gmail.com>
2016-06-06 17:24:45 +0200

    Suite entity

--- src/AppBundle/Entity/module/Proposal.php
+++ src/AppBundle/Entity/module/Proposal.php
@@ -21,7 +21,13 @@ class Proposal
      */
     private $id;
 
-
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="ProposalElement", mappedBy="proposal")
+     */
+    private $proposalElements;
+    
     /**
      * Get id
      *
@@ -30,5 +36,46 @@ class Proposal
     public function getId()
     {
         return $this->id;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->proposalElements = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add proposalElement
+     *
+     * @param \AppBundle\Entity\module\ProposalElement $proposalElement
+     *
+     * @return Proposal
+     */
+    public function addProposalElement(\AppBundle\Entity\module\ProposalElement $proposalElement)
+    {
+        $this->proposalElements[] = $proposalElement;
+
+        return $this;
+    }
+
+    /**
+     * Remove proposalElement
+     *
+     * @param \AppBundle\Entity\module\ProposalElement $proposalElement
+     */
+    public function removeProposalElement(\AppBundle\Entity\module\ProposalElement $proposalElement)
+    {
+        $this->proposalElements->removeElement($proposalElement);
+    }
+
+    /**
+     * Get proposalElements
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProposalElements()
+    {
+        return $this->proposalElements;
     }
 }
