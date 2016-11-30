314753063e3bfb547317877445516f2d67b7922c
Flavien Audin <flavien.audin@gmail.com>
2016-11-30 15:55:43 +0100

    PollModule Where : - autocomplétion des champs "Where" avec GooglePlaceId - Stockage en BDD du GooglePlaceId

--- src/AppBundle/Utils/enum/PollElementType.php
+++ src/AppBundle/Utils/enum/PollElementType.php
@@ -12,10 +12,10 @@ namespace AppBundle\Utils\enum;
 class PollElementType extends AbstractEnumType
 {
 
-    const HIDDEN ="hidden";
+    const GOOGLE_PLACE_ID ="googlePlaceId";
     // Values already in AbstractEnumType's constantes
 
     protected $name = 'enum_pollproposal_elementtype';
-    protected $values = array(self::STRING, self::INTEGER, self::DATETIME, self::HIDDEN);
+    protected $values = array(self::STRING, self::INTEGER, self::DATETIME, self::GOOGLE_PLACE_ID);
 
 }
