b77c2b3df249780f897a63c657e3b3bce6585b35
Flavien Audin <flavien.audin@gmail.com>
2017-01-19 15:47:57 +0100

    Titre d'un événement  obligatoire

--- src/AppBundle/Form/Event/EventType.php
+++ src/AppBundle/Form/Event/EventType.php
@@ -17,6 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EventType extends AbstractType
 {
@@ -24,7 +25,10 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("name")
+            ->add("name", TextType::class, array(
+                'required' => true,
+                'constraints' => new NotBlank()
+            ))
             ->add("description", TextareaType::class, array(
                     'required' => false
                 )
