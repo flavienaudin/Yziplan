b0f668d5d3f0ba823f9c5a9b7ebaef14cef0dd82
Patrick Massé <patrickmbox@gmail.com>
2017-03-28 09:41:44 +0200

    Restauration PollProposalElement

--- src/AppBundle/Entity/Module/PollProposalElement.php
+++ src/AppBundle/Entity/Module/PollProposalElement.php
@@ -29,7 +29,96 @@ class PollProposalElement
      */
     private $id;
 
+    /**
+     * Contient la valeur si le PollElement correspondant est de type
+     *
+     * @var string
+     * @ORM\Column(name="val_string", type="string", length=511, nullable=true)
+     */
+    private $valString;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="val_text", type="text", nullable=true)
+     */
+    private $valText;
+
+    /**
+     * Contient la valeur si le type == PollElementType::INTEGER
+     *
+     * @var integer
+     *
+     * @ORM\Column(name="val_integer", type="integer", nullable=true)
+     */
+    private $valInteger;
+
+    /**
+     * Contient la valeur si le type == PollElementType::DATETIME
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(name="val_datetime", type="datetime", nullable=true)
+     */
+    private $valDatetime;
+
+    /**
+     * true si on doit prendre en compte l'heure de valDateTime
+     *
+     * @var boolean
+     *
+     * @ORM\Column(name="has_time", type="boolean", nullable=false)
+     */
+    private $time = false;
+
+    /**
+     * Contient la valeur si le type == PollElementType::DATETIME
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(name="val_enddatetime", type="datetime", nullable=true)
+     */
+    private $valEndDatetime;
+
+    /**
+     * true si on doit prendre en compte la date de valEndDateTime
+     *
+     * @var boolean
+     *
+     * @ORM\Column(name="has_end_date", type="boolean", nullable=false)
+     */
+    private $endDate = false;
+
+    /**
+     * true si on doit prendre en compte l'heure de valEndDateTime
+     *
+     * @var boolean
+     *
+     * @ORM\Column(name="has_end_time", type="boolean", nullable=false)
+     */
+    private $endTime = false;
+
+    /**
+     * Contient la valeur du Google Place Id si le PollElement est de Type PollElementType::GOOGLE_PLACE_ID
+     *
+     * @var string
+     * @ORM\Column(name="val_google_place_id", type="string", length=511, nullable=true)
+     */
+    private $valGooglePlaceId;
+
+    /**
+     * This attributes stores the filename of the file for the database
+     * @var string
+     * @ORM\Column(name="picture_filename", type="string", length=255, nullable=true)
+     */
+    private $pictureFilename;
+
+    /**
+     * This is not a mapped field of entity metadata, just a simple property.
+     * @Vich\UploadableField(mapping="pollproposalelement_picture", fileNameProperty="pictureFilename")
+     * @var File
+     */
+    private $pictureFile;
 
     /***********************************************************************
      *                      Jointures
@@ -65,7 +154,206 @@ class PollProposalElement
         return $this->id;
     }
 
+    /**
+     * @return string
+     */
+    public function getValString()
+    {
+        return $this->valString;
+    }
 
+    /**
+     * @param string $valString
+     * @return PollProposalElement
+     */
+    public function setValString($valString)
+    {
+        $this->valString = $valString;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValText()
+    {
+        return $this->valText;
+    }
+
+    /**
+     * @param string $valText
+     */
+    public function setValText($valText)
+    {
+        $this->valText = $valText;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getValInteger()
+    {
+        return $this->valInteger;
+    }
+
+    /**
+     * @param int $valInteger
+     * @return PollProposalElement
+     */
+    public function setValInteger($valInteger)
+    {
+        $this->valInteger = $valInteger;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getValDatetime()
+    {
+        return $this->valDatetime;
+    }
+
+    /**
+     * @param \DateTime $valDatetime
+     * @return PollProposalElement
+     */
+    public function setValDatetime($valDatetime)
+    {
+        $this->valDatetime = $valDatetime;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param bool $hasTime
+     */
+    public function setTime($hasTime)
+    {
+        $this->time = $hasTime;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getValEndDatetime()
+    {
+        return $this->valEndDatetime;
+    }
+
+    /**
+     * @param \DateTime $valEndDatetime
+     */
+    public function setValEndDatetime($valEndDatetime)
+    {
+        $this->valEndDatetime = $valEndDatetime;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param bool $endDate
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function hasEndTime()
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * @param bool $hasEndTime
+     */
+    public function setEndTime($hasEndTime)
+    {
+        $this->endTime = $hasEndTime;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getValGooglePlaceId()
+    {
+        return $this->valGooglePlaceId;
+    }
+
+    /**
+     * @param string $valGooglePlaceId
+     * @return PollProposalElement
+     */
+    public function setValGooglePlaceId($valGooglePlaceId)
+    {
+        $this->valGooglePlaceId = $valGooglePlaceId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPictureFilename()
+    {
+        return $this->pictureFilename;
+    }
+
+    /**
+     * @param string $pictureFilename
+     * @return PollProposalElement
+     */
+    public function setPictureFilename($pictureFilename)
+    {
+        $this->pictureFilename = $pictureFilename;
+        return $this;
+    }
+
+    /**
+     * @return File
+     */
+    public function getPictureFile()
+    {
+        return $this->pictureFile;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $pictureFile
+     * @return PollProposalElement
+     */
+    public function setPictureFile($pictureFile)
+    {
+        $this->pictureFile = $pictureFile;
+        if ($pictureFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+        return $this;
+    }
 
     /**
      * @return PollProposal
@@ -118,6 +406,9 @@ class PollProposalElement
 
             case PollElementType::DATETIME:
                 $val = $this->getValDatetime();
+                break;
+            case PollElementType::END_DATETIME:
+                $val = $this->getValEndDatetime();
                 break;
             case PollElementType::INTEGER :
                 $val = $this->getValInteger();
