7915e8518c603c7564482b64dd86a2ccf316012f
Flavien Audin <flavien.audin@gmail.com>
2017-03-31 12:50:29 +0200

    correction repertoires des pollproposal

--- src/AppBundle/Utils/Naming/PictureDirectoryNamer.php
+++ src/AppBundle/Utils/Naming/PictureDirectoryNamer.php
@@ -10,7 +10,7 @@ namespace AppBundle\Utils\Naming;
 
 
 use AppBundle\Entity\Event\Event;
-use AppBundle\Entity\Module\PollProposalElement;
+use AppBundle\Entity\Module\PollProposal;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
 use Vich\UploaderBundle\Naming\DirectoryNamerInterface;
 
@@ -23,15 +23,15 @@ class PictureDirectoryNamer implements DirectoryNamerInterface
     {
         $subdir = 'unkownobject';
         if ($object instanceof Event) {
-            if($object->getCreatedAt() != null) {
+            if ($object->getCreatedAt() != null) {
                 $subdir = date("Y/m/", $object->getCreatedAt()->getTimestamp()) . $object->getToken();
-            }else{
+            } else {
                 $subdir = date("Y/m/") . $object->getToken();
             }
-        }elseif ($object instanceof PollProposalElement) {
-            if($object->getCreatedAt() != null) {
+        } elseif ($object instanceof PollProposal) {
+            if ($object->getCreatedAt() != null) {
                 $subdir = date("Y/m/", $object->getCreatedAt()->getTimestamp());
-            }else{
+            } else {
                 $subdir = date("Y/m/");
             }
         }
